567f41ea52a95575dd1c4a418eab6ad2e7263853
ArnabTo <arnabs929@gmail.com>
2024-05-21 01:00:05 +0600

    updated ads section

--- components/ads-section/AdsSection.js
+++ components/ads-section/AdsSection.js
@@ -1,15 +1,23 @@
+'use client';
 import Image from "next/image";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "../ui/carousel";
+import Autoplay from 'embla-carousel-autoplay'
+import { useRef } from "react";
 const AdsSection = () => {
+
+    const plugin = useRef(
+        Autoplay({ delay: 2000, stopOnInteraction: true })
+      )
+
     return (
         <div className="max-w-4xl mx-auto my-20">
-            <Carousel>
+            <Carousel plugins={[plugin.current]} >
                 <CarouselContent>
                     <CarouselItem>
-                        <Image src='https://i.ibb.co/nfZQ6Pt/White-Minimalist-Corporate-Business-Personal-Profile-Linked-In-Banner.png' width={1000} height={500} className='w-full max-h-[50%] rounded-t-lg' />
+                        <Image src='https://i.ibb.co/nfZQ6Pt/White-Minimalist-Corporate-Business-Personal-Profile-Linked-In-Banner.png' width={1000} height={500} className='w-full max-h-[50%] rounded-2xl' />
                     </CarouselItem>
                     <CarouselItem>
-                        <Image src='https://i.ibb.co/4JYGNdV/Grey-Minimalist-Special-Offer-Banner-Landscape.png' width={1000} height={500} className='w-full max-h-[50%] object-cover rounded-t-lg' />
+                        <Image src='https://i.ibb.co/4JYGNdV/Grey-Minimalist-Special-Offer-Banner-Landscape.png' width={1000} height={500} className='w-full max-h-[50%] object-cover rounded-2xl' />
                     </CarouselItem>
                 </CarouselContent>
             </Carousel>
---
+++ components/blogs/BlogList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlogList = () => {
+    return (
+        <div>
+            <h2>All Blogs will be here</h2>
+        </div>
+    );
+};
+
+export default BlogList;
